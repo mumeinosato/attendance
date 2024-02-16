353c34d7d3670954da32e839e4c181e14aa63daf
mumeinosato <66110797+mumeinosato@users.noreply.github.com>
2024-02-17 07:52:51 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+  <script setup lang="ts">
+  import { RouterLink, RouterView } from 'vue-router'
+  import Header from './common/Header.vue'
+  import Footer from './common/Footer.vue'
+  import 'vuetify/dist/vuetify.min.css'
+  </script>
+
+<template>
+  <header>
+    <Header />
+    <!--<img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>-->
+  </header>
+  <RouterView />
+  <footer>
+    <Footer />
+  </footer>
+</template>
+
+<style>
+body {
+  margin: 0;
+}
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/common/Header.vue
@@ -0,0 +1,61 @@
+<template>
+  <header>
+    <div id="app">
+      <div class="hamburger" v-if="isMobile">
+        <nav>
+          <button type="btn" class="nav-btn" v-on:click="open = !open">
+            <i class="fas fa-bars" aria-hidden="true"></i>
+          </button>
+          <div class="nav-items" v-bind:class="{ 'is-active': open }">
+            <div class="nav-item">Home</div>
+            <div class="nav-item">About</div>
+            <div class="nav-item">Topic</div>
+          </div>
+        </nav>
+      </div>
+
+      <div class="pc-menu" v-if="!isMobile">
+        <nav>
+          <ul class="main-nav">
+            <li>
+              <router-link to="/"><img src="../assets/logo.svg" /></router-link>
+            </li>
+            <li><router-link to="/">ホーム</router-link></li>
+            <li><router-link to="/schedule">自己紹介</router-link></li>
+            <li><router-link to="/group">ブログ</router-link></li>
+            <li><router-link to="/group">Discord Bot</router-link></li>
+            <li><router-link to="/map">サーバーステータス</router-link></li>
+            <li><router-link to="/notice">お問い合わせ</router-link></li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  </header>
+</template>
+    
+  <script>
+export default {
+  data() {
+    return {
+      title: "ヘッダー",
+      isMobile: false,
+      drawer: false,
+    };
+  },
+  mounted() {
+    this.checkMobile();
+    window.addEventListener("resize", this.checkMobile);
+  },
+  beforeDestroy() {
+    window.removeEventListener("resize", this.checkMobile);
+  },
+  methods: {
+    checkMobile() {
+      this.isMobile = window.innerWidth < 768;
+    },
+  },
+};
+</script>
+    
+    <style scoped>
+</style>
